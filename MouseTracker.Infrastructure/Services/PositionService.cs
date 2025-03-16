034bb621a4b2ab4f73fa579bf15a43b9d4c0c12a
magomed <magomedyasulov8@gmail.com>
2025-03-16 03:09:26 +0300

    add: Реализация IPositionService

--- MouseTracker.Infrastructure/Services/PositionService.cs
+++ MouseTracker.Infrastructure/Services/PositionService.cs
@@ -1,29 +1,68 @@
-﻿using MouseTracker.Application.Abstractions;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using MouseTracker.Application.Abstractions;
 using MouseTracker.Application.DTOs.Request;
 using MouseTracker.Application.DTOs.Resposne;
+using MouseTracker.Domain.Data;
+using MouseTracker.Domain.Data.Entites;
+using MouseTracker.Domain.Exceptions;
+using System.Linq.Expressions;
 
 namespace MouseTracker.Infrastructure.Services
 {
     public class PositionService : IPositionsService
     {
-        public Task<PositionDto[]> Create(CreatePositionDto[] positionsDto)
+        private readonly IMapper _mapper;
+        private readonly ApplicationContext _dbContext;
+
+        public PositionService(
+            IMapper mapper, 
+            ApplicationContext dbContext)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _dbContext = dbContext;
         }
 
-        public Task Delete(int id)
+        public async Task<PositionDto> Get(int id)
         {
-            throw new NotImplementedException();
+            var position = await _dbContext.Positions.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+            if (position == null)
+                throw new ServiceException("Position Not Found", $"Position with id {id} not found.", StatusCodes.Status404NotFound);
+
+            return _mapper.Map<PositionDto>(position);
         }
 
-        public Task<PositionDto> Get(int id)
+        public async Task<PositionDto[]> Get(PositionFilter filter)
         {
-            throw new NotImplementedException();
+            var isStartTimeNull = filter.StartTime == null;
+            var isEndTimeNull = filter.EndTime == null;
+
+            Expression<Func<Position, bool>> exp = r => ((isStartTimeNull || r.CreatedAt >= filter.StartTime) &&
+                                                         (isEndTimeNull || r.CreatedAt <= filter.EndTime));
+
+            var positions = await _dbContext.Positions.AsNoTracking().Where(exp).ToArrayAsync();
+            return _mapper.Map<PositionDto[]>(positions);
         }
 
-        public Task<PositionDto[]> Get(PositionFilter filter)
+        public async Task<PositionDto[]> Create(CreatePositionDto[] models)
         {
-            throw new NotImplementedException();
+            var positions = _mapper.Map<Position[]>(models);
+            await _dbContext.Positions.AddRangeAsync(positions);
+            await _dbContext.SaveChangesAsync();
+
+            var positionsDto = _mapper.Map<PositionDto[]>(positions);
+            return positionsDto;
+        }
+ 
+        public async Task Delete(int id)
+        {
+            var position = await _dbContext.Positions.FirstOrDefaultAsync(m => m.Id == id);
+            if (position == null)
+                throw new ServiceException("Position Not Found", $"Position with id {id} not found.", StatusCodes.Status404NotFound);
+
+            _dbContext.Positions.Remove(position);
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
